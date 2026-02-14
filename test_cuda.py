2fa237cc087be4f57173812a097fa8060876c021
chichirita <143538703+chichirita@users.noreply.github.com>
2026-02-14 21:39:19 +0300

    Update test_cuda.py

--- test_cuda.py
+++ test_cuda.py
@@ -1,3 +1,3 @@
 import torch
-print(torch.version.cuda)          # Версия CUDA, с которой собран PyTorch
-print(torch.cuda.is_available())   # True
+print(torch.version.cuda)          
+print(torch.cuda.is_available())
